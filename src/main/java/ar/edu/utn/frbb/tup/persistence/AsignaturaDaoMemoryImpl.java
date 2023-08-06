f69d5343537918aa3e2647eabb859d4761ff7053
Gonzalo <113151385+GonzaloStickar@users.noreply.github.com>
2023-08-06 20:17:44 -0300

    fix Correlatividades static (prog 3 / 2 no tenían)

--- src/main/java/ar/edu/utn/frbb/tup/persistence/AsignaturaDaoMemoryImpl.java
+++ src/main/java/ar/edu/utn/frbb/tup/persistence/AsignaturaDaoMemoryImpl.java
@@ -1,6 +1,5 @@
 package ar.edu.utn.frbb.tup.persistence;
 
-import ar.edu.utn.frbb.tup.business.ProfesorService;
 import ar.edu.utn.frbb.tup.model.Asignatura;
 import ar.edu.utn.frbb.tup.model.Materia;
 import ar.edu.utn.frbb.tup.model.Profesor;
@@ -19,17 +18,23 @@ public class AsignaturaDaoMemoryImpl implements AsignaturaDao {
     static {
         repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 1",1,1, new Profesor("Pedro","Sánchez","Lic. COMPUTACIÓN"))));
         repositorioAsignaturas.add(new Asignatura(new Materia("Programación 1",1,1, new Profesor("Pepe","Sierra","Ing. COMPUTACIÓN"))));
-        repositorioAsignaturas.add(new Asignatura(new Materia("Programación 2",1,1, new Profesor("Vicenta","Peiro","Ing. COMPUTACIÓN"))));
-        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 2",1,1,new Profesor("Ismael","Montesinos","Lic. COMPUTACIÓN"))));
-        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 3",1,1, new Profesor("Luciano","Salotto","Lic. COMPUTACIÓN")))); //The best! :)
-        repositorioAsignaturas.add(new Asignatura(new Materia("Programación 3",1,1, new Profesor("Manuela","Rivera","Ing. COMPUTACIÓN"))));
-        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 4",1,1, new Profesor("Gregorio","Barbero","Lic. COMPUTACIÓN"))));
+        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 2",1,2,new Profesor("Ismael","Montesinos","Lic. COMPUTACIÓN"))));
+        repositorioAsignaturas.add(new Asignatura(new Materia("Programación 2",2,1, new Profesor("Vicenta","Peiro","Ing. COMPUTACIÓN"))));
+        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 3",2,1, new Profesor("Luciano","Salotto","Lic. COMPUTACIÓN")))); //The best! :)
+        repositorioAsignaturas.add(new Asignatura(new Materia("Programación 3",2,1, new Profesor("Manuela","Rivera","Ing. COMPUTACIÓN"))));
+        repositorioAsignaturas.add(new Asignatura(new Materia("Laboratorio 4",2,2, new Profesor("Gregorio","Barbero","Lic. COMPUTACIÓN"))));
 
         int i=0;
         for (Asignatura asignatura : repositorioAsignaturas) {
-            if (i>0 && i%2==0) {
-                asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get((i-1)).getMateria().getNombre());
-                asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get((i-2)).getMateria().getNombre());
+            if (i>0 && i%2==0 || asignatura.getMateria().getNombre().equals("Programación 3") || asignatura.getMateria().getNombre().equals("Programación 2")) {
+                if (asignatura.getMateria().getNombre().equals("Programación 3") || asignatura.getMateria().getNombre().equals("Programación 2")) {
+                    asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get(i-2).getMateria().getNombre());
+                    asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get(i-3).getMateria().getNombre());
+                }
+                else {
+                    asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get(i-1).getMateria().getNombre());
+                    asignatura.getMateria().agregarCorrelatividad(repositorioAsignaturas.get(i-2).getMateria().getNombre());
+                }
             }
             asignatura.getMateria().setMateriaId(i);
             asignatura.getMateria().getProfesor().setprofesorId(i);
